527a41b2deb5b7b52bc879d89e0cb0d91f2dc977
kair <draste528@gmail.com>
2026-07-26 21:42:23 +0300

    Initial Commit

---
+++ InternetShopTests/InternetShopTests.swift
@@ -0,0 +1,19 @@
+//
+//  InternetShopTests.swift
+//  InternetShopTests
+//
+//  Created by kair on 26.07.26.
+//
+
+import Testing
+@testable import InternetShop
+
+struct InternetShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
