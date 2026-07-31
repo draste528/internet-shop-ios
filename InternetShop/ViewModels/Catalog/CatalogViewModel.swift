9f7b4ecadc510b17003c53316d9c08e5522a3db7
kair <draste528@gmail.com>
2026-07-31 16:16:34 +0300

    green / gray tabs (icon , text)

---
+++ InternetShop/ViewModels/Catalog/CatalogViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  CatalogView.swift
+//  InternetShop
+//
+//  Created by kair on 29.07.26.
+//
+
+import SwiftUI
+
+struct CatalogView: View {
+
+    var body: some View {
+        Text("Catalog")
+    }
+}
+
+
+#Preview {
+    CatalogView()
+}
---
+++ InternetShop/ViewModels/Settings/SettingsViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  InternetShop
+//
+//  Created by kair on 29.07.26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+
+#Preview {
+    SettingsView()
+}
